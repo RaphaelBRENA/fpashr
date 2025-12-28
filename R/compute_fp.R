ffc87e295ad3bd6e0937391c20c4fc032157fd6b
ConorForan3 <conor.foran.2023@mumail.ie>
2025-12-28 21:05:07 +0000

    created frequency polygon function and made unit tests

---
+++ R/compute_fp.R
@@ -0,0 +1,48 @@
+#' Compute Frequency Polygons
+#'
+#' Calculates the values needed to construct a frequency polygon from numeric data, without producing a plot
+#'
+#' @importFrom graphics hist
+#'
+#' @param x A numeric vector of observations
+#' @param bins The number of bins used to construct the histogram
+#' @param density Logical; if TRUE returns density instead of counts.
+#'
+#' @return An object of class \code{"frequency_polygon"} containing:
+#'   \itemize{
+#'   \item mids: Centerpoints of each histogram bin
+#'   \item y: Counts or densities at each midpoint
+#'   \item binwidth: The width of the bins
+#'}
+#' @examples
+#' data(faithful)
+#' fp <- compute_fp(faithful$waiting, bins = 10)
+#' fp$mids
+#'
+#' @author Conor Foran - <\email{conor.foran.2023@@mumail.ie}
+#'
+#'
+#'
+#' @export
+compute_fp <- function(x, bins = 10, density = TRUE) {
+  if (!is.numeric(x)) {
+    stop("x must be a numeric vector")
+  }
+
+  if (length(x) < 2) {
+    stop("x must contain at least two observations")
+  }
+  breaks <- seq(min(x, na.rm = TRUE), max(x, na.rm = TRUE), length.out = bins + 1)
+  h <- hist(x, breaks = breaks, plot = FALSE)
+
+
+  y <- if (density) h$density else h$counts
+
+  result <- list(
+    mids = h$mids,
+    y = y,
+    binwidth = diff(h$breaks)[1]
+  )
+  class(result) <- "frequency_polygon"
+  result
+}
---
+++ tests/testthat/test_compute_fp.R
@@ -0,0 +1,36 @@
+library(testthat)
+test_that("compute_fp returns correct structure", {
+  data(faithful)
+
+  fp <- compute_fp(faithful$waiting, bins = 10)
+
+  expect_s3_class(fp, "frequency_polygon")
+  expect_type(fp$mids, "double")
+  expect_type(fp$y, "double")
+  expect_length(fp$mids, 10)
+  expect_length(fp$y, 10)
+})
+
+test_that("density integrates approximately to 1", {
+  data(faithful)
+
+  fp <- compute_fp(faithful$waiting, bins = 20)
+
+  area <- sum(fp$y * fp$binwidth)
+  expect_equal(area, 1, tolerance = 0.05)
+})
+
+test_that("non-numeric input throws error", {
+  expect_error(
+    compute_fp(c("a", "b", "c")),
+    "numeric"
+  )
+})
+
+test_that("too few observations throws error", {
+  expect_error(
+    compute_fp(1),
+    "at least two"
+  )
+})
+
