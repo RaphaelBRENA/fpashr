c7185f89a6aad133297152f2dbdee89a15466062
ConorForan3 <conor.foran.2023@mumail.ie>
2026-01-09 21:46:43 +0000

    Updated my unit tests for my compute_fp function

--- tests/testthat/test_compute_fp.R
+++ tests/testthat/test_compute_fp.R
@@ -1,33 +1,26 @@
 library(testthat)
-test_that("compute_fp returns correct structure", {
-  data(faithful)
+library(MASS)
+test_that("compute_fp returns correct form", {
+  data(geyser)
 
-  fp <- compute_fp(faithful$waiting, bins = 10)
+  fp <- compute_fp(geyser$waiting, bins = 10)
 
   expect_s3_class(fp, "frequency_polygon")
-  expect_type(fp$mids, "double")
+  expect_type(fp$x, "double")
   expect_type(fp$y, "double")
-  expect_length(fp$mids, 10)
+  expect_length(fp$x, 10)
   expect_length(fp$y, 10)
 })
 
-test_that("density integrates approximately to 1", {
-  data(faithful)
 
-  fp <- compute_fp(faithful$waiting, bins = 20)
-
-  area <- sum(fp$y * fp$binwidth)
-  expect_equal(area, 1, tolerance = 0.05)
-})
-
-test_that("non-numeric input throws error", {
+test_that("Inputs that arent numeric gives error", {
   expect_error(
     compute_fp(c("a", "b", "c")),
     "numeric"
   )
 })
 
-test_that("too few observations throws error", {
+test_that("Not enough observations gives error", {
   expect_error(
     compute_fp(1),
     "at least two"
